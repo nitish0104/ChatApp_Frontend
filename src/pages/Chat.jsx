5cd0ae1e4d401ae7650d375eb1bf2e2ebd5bbff3
nitish0104 <nitishdalvi1@gmail.com>
2023-06-03 22:14:44 +0530

    useeffect

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,16 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { ChatState } from "../Context/chatProvider";
+import axios from "axios";
 const Chat = () => {
   const { user } = ChatState();
+  const [NoteCloud, setNoteCloud] = useState("");
+  // const [refresh, setrefresh] = useState(false);
 
   useEffect(() => {
-    console.log(user);
+    try {
+      axios("http://localhost:5000/api/user/signin/", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+        .then((res) => {
+          setNoteCloud(res.data);
+          console.log(res.data);
+        })
+        .catch((err) => console.log(err));
+    } catch (error) {
+      console.log(error);
+    }
+    console.log("useeffect");
+    // eslint-disable-next-line
   }, []);
 
   return (
     <div>
       <div>hello</div>
-      <div>{user.user.username}</div>
+      <div>{NoteCloud.username}</div>
       <div>{user.user.email}</div>
     </div>
   );
